b617222e036fe5cf11177629e506b083e62b4a67
wafflu <uwpgus@naver.com>
2024-07-25 14:16:31 +0900

    :memo: Modify : 인터프리터 readme 내용추가

---
+++ src/p23_Interpreter/Node.java
@@ -0,0 +1,5 @@
+package p23_Interpreter;
+
+public abstract class Node {
+    public abstract void parse(Context context) throws ParseException;
+}
---
+++ src/p23_Interpreter/ParseException.java
@@ -0,0 +1,7 @@
+package p23_Interpreter;
+
+public class ParseException extends  Exception{
+    public ParseException(String msg){
+        super(msg);
+    }
+}
